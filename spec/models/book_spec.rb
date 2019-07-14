6905946127367f8882a80c84a7cea7da54a3f1b9
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-07-14 16:51:51 -0600

    Add relationship and validations for many-to-many relationship

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Book do
     it { should validate_presence_of :page_count }
     it { should validate_presence_of :year_published }
   end
+
+  describe 'relationships' do
+    it { should have_many :authors }
+    it { should have_many(:authors).through(:book_authors)}
+  end
 end
