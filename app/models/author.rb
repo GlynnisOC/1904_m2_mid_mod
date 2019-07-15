90ff7b452d49403dc4af253ad7c38342a4865297
Glynnis O'Connell <oconnellg1717@gmail.com>
2019-07-14 21:54:48 -0600

    Add name, book titles, and average number of pages written to author show page

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,8 @@ class Author < ApplicationRecord
   validates_presence_of :name
   has_many :book_authors
   has_many :books, through: :book_authors
+
+  def average_pages_written
+    books.average(:page_count)
+  end
 end
